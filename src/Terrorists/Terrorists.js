bc525d58eab5451eb95ca04ca5282ee84758a495
Dmitry Kochnev <civil.dinosaur@gmail.com>
2024-03-14 23:20:25 +0400

    new terrorists

---
+++ src/Terrorists/Terrorists.js
@@ -0,0 +1,9 @@
+import styles from './Terrorists.module.css';
+
+export const Terrorists = () => {
+  return (
+    <div className={styles.root}>
+      <img className={styles.image} src="/terrorists.png" alt="terrorists" />
+    </div>
+  );
+};
